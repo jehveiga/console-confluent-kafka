51b66553954c2e50ac841b1ed8736dee4c5aed6c
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-08-10 11:34:23 -0300

    Add project files.

---
+++ Console.Confluent.Kafka.Consumidor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "consumer-teste01",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+// Se increvendo na fila
+consumer.Subscribe("topico-teste");
+
+while (true)
+{
+    // Será efetuado a leitura das mensagens, ela será lida uma a uma dentro do looping
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: {result.Message.Key} - {result.Message.Value}");
+}
---
+++ Console.Confluent.Kafka.Produtor/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Teste de mensagem {DateTime.Now.Second}"
+};
+
+
+var result = await producer.ProduceAsync(topic: "topico-teste", message: message);
+
+
+Console.WriteLine($"{result.Offset}");
